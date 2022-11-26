51291273c0330fd7c13b58793967393fee4b03bf
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-11-26 17:05:31 -0300

    quest 02 em progresso

--- quest-02-html-css-js-formulario/src/js/script.js
+++ quest-02-html-css-js-formulario/src/js/script.js
@@ -1,4 +1,28 @@
 const inputs = document.querySelectorAll("input")
-const textArea = document.getElementsByTagName("textarea")
+const textArea = document.querySelectorAll("textarea")
+const btnEnviar = document.querySelector(".btn-enviar");
 
 
+
+btnEnviar.addEventListener("click", function(){
+    inputs.forEach(function(item){
+        if(item.value == ""){
+            item.classList.add("borda-vermelha")
+        } else {
+            item.classList.add("campo-preenchido")
+        }
+    })
+
+    textArea.forEach(function(item){
+        console.log(item)
+        if(item.value == ""){
+            item.classList.add("borda-vermelha")
+        } else {
+            item.classList.add("campo-preenchido")
+        }
+    })
+})
+
+btnEnviar.addEventListener("click", function(){
+
+})
