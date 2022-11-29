c45a76b0ccd77bef414540c1d40253c6cf68716f
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-11-29 16:42:02 -0300

    quest 02 concluida

--- quest-02-html-css-js-formulario/src/js/script.js
+++ quest-02-html-css-js-formulario/src/js/script.js
@@ -1,28 +1,37 @@
-const inputs = document.querySelectorAll("input")
-const textArea = document.querySelectorAll("textarea")
-const btnEnviar = document.querySelector(".btn-enviar");
+const formControl = document.querySelectorAll(".form-control")
+const inputNome = document.getElementById("nome")
+const inputEmail = document.getElementById("email")
+const inputTelefone = document.getElementById("telefone")
+const textArea = document.getElementById("mensagem")
+const btnEnviar = document.querySelector(".btn-enviar")
 
-
-
-btnEnviar.addEventListener("click", function(){
-    inputs.forEach(function(item){
-        if(item.value == ""){
-            item.classList.add("borda-vermelha")
-        } else {
-            item.classList.add("campo-preenchido")
-        }
-    })
-
-    textArea.forEach(function(item){
-        console.log(item)
-        if(item.value == ""){
-            item.classList.add("borda-vermelha")
-        } else {
-            item.classList.add("campo-preenchido")
-        }
-    })
-})
+console.log(formControl)
+console.log(inputNome)
 
 btnEnviar.addEventListener("click", function(){
+    if(inputNome.value === ""){
+        formControl[0].classList.add("error")
+    } else {
+        formControl[0].classList.add("success")
+    }
 
+    if(inputEmail.value === ""){
+        formControl[1].classList.add("error")
+    } else {
+        formControl[1].classList.add("success")
+    }
+
+    if(inputTelefone.value === ""){
+        formControl[2].classList.add("error")
+    } else {
+        formControl[2].classList.add("success")
+    }
+
+    if(textArea.value === ""){
+        formControl[3].classList.add("error")
+    } else {
+        formControl[3].classList.add("success")
+    }
 })
+
+
